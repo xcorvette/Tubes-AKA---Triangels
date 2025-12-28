e6d2ae8c55a0ccee2de78bf0f2390019a543527d
hilwaaulianajaaha <hilwaaulianajaaha@student.telkomuniversity.ac.id>
2025-12-28 22:14:35 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,34 +1,29 @@
 #include <iostream>
 using namespace std;
 
+void generateData(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        arr[i] = (i + 1) * 2;
+}
+
 int binarySearchIterative(int arr[], int n, int target, int &steps) {
     int left = 0, right = n - 1;
     steps = 0;
-
     while (left <= right) {
         steps++;
         int mid = left + (right - left) / 2;
-
-        if (arr[mid] == target)
-            return mid;
-        else if (arr[mid] < target)
-            left = mid + 1;
-        else
-            right = mid - 1;
+        if (arr[mid] == target) return mid;
+        else if (arr[mid] < target) left = mid + 1;
+        else right = mid - 1;
     }
     return -1;
 }
 
 int binarySearchRecursive(int arr[], int left, int right, int target, int &steps) {
     steps++;
-
-    if (left > right)
-        return -1;
-
+    if (left > right) return -1;
     int mid = left + (right - left) / 2;
-
-    if (arr[mid] == target)
-        return mid;
+    if (arr[mid] == target) return mid;
     else if (arr[mid] < target)
         return binarySearchRecursive(arr, mid + 1, right, target, steps);
     else
@@ -36,31 +31,21 @@ int binarySearchRecursive(int arr[], int left, int right, int target, int &steps
 }
 
 int main() {
-    int data[] = {10, 20, 30, 40, 50, 60, 70};
-    int n = sizeof(data) / sizeof(data[0]);
+    const int N = 10000;
+    int data[N];
+    generateData(data, N);
 
-    int key;
-    cout << "Masukkan angka yang dicari: ";
-    cin >> key;
+    int target;
+    cout << "Masukkan ID produk: ";
+    cin >> target;
 
-    int stepsIter = 0, stepsRec = 0;
+    int stepsI = 0, stepsR = 0;
+    int idxI = binarySearchIterative(data, N, target, stepsI);
+    int idxR = binarySearchRecursive(data, 0, N - 1, target, stepsR);
 
-    int resultIter = binarySearchIterative(data, n, key, stepsIter);
-    int resultRec = binarySearchRecursive(data, 0, n - 1, key, stepsRec);
-
-    cout << "\nHasil Iteratif: ";
-    if (resultIter != -1)
-        cout << "Ditemukan di index " << resultIter;
-    else
-        cout << "Tidak ditemukan";
-    cout << " | Steps: " << stepsIter << endl;
-
-    cout << "Hasil Rekursif: ";
-    if (resultRec != -1)
-        cout << "Ditemukan di index " << resultRec;
-    else
-        cout << "Tidak ditemukan";
-    cout << " | Steps: " << stepsRec << endl;
+    cout << "\n=== HASIL ===\n";
+    cout << "Iteratif  -> Index: " << idxI << ", Steps: " << stepsI << endl;
+    cout << "Rekursif -> Index: " << idxR << ", Steps: " << stepsR << endl;
 
     return 0;
 }
