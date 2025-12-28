fe689cd570d3569fc88eb8b79785091a062bd94f
hilwaaulianajaaha <hilwaaulianajaaha@studnt.telkomuniversity.ac.id>
2025-12-28 21:24:21 +0700

    Update main.cpp: iterative and recursive binary search with dummy data

--- main.cpp
+++ main.cpp
@@ -18,3 +18,49 @@ int binarySearchIterative(int arr[], int n, int target, int &steps) {
     }
     return -1;
 }
+
+int binarySearchRecursive(int arr[], int left, int right, int target, int &steps) {
+    steps++;
+
+    if (left > right)
+        return -1;
+
+    int mid = left + (right - left) / 2;
+
+    if (arr[mid] == target)
+        return mid;
+    else if (arr[mid] < target)
+        return binarySearchRecursive(arr, mid + 1, right, target, steps);
+    else
+        return binarySearchRecursive(arr, left, mid - 1, target, steps);
+}
+
+int main() {
+    int data[] = {10, 20, 30, 40, 50, 60, 70};
+    int n = sizeof(data) / sizeof(data[0]);
+
+    int key;
+    cout << "Masukkan angka yang dicari: ";
+    cin >> key;
+
+    int stepsIter = 0, stepsRec = 0;
+
+    int resultIter = binarySearchIterative(data, n, key, stepsIter);
+    int resultRec = binarySearchRecursive(data, 0, n - 1, key, stepsRec);
+
+    cout << "\nHasil Iteratif: ";
+    if (resultIter != -1)
+        cout << "Ditemukan di index " << resultIter;
+    else
+        cout << "Tidak ditemukan";
+    cout << " | Steps: " << stepsIter << endl;
+
+    cout << "Hasil Rekursif: ";
+    if (resultRec != -1)
+        cout << "Ditemukan di index " << resultRec;
+    else
+        cout << "Tidak ditemukan";
+    cout << " | Steps: " << stepsRec << endl;
+
+    return 0;
+}
